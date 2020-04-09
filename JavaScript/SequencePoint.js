5615d6daa2d2ae63b1e706a0e9205971c59f2049
Prince Yadav <prince.yadavp098@gmail.com>
2020-04-09 19:10:22 +0530

    by me

--- JavaScript/SequencePoint.js
+++ JavaScript/SequencePoint.js
@@ -1,11 +1,11 @@
 var x = 1 ;
 var y = 2 ;
 
-//problem with Sequence point 
+//Problem with Sequence point 
 x^=y^=x^=y;
 console.log(x,y)
 
-// No problem with sequence point 
+// No problem with Sequence Point 
 x^=y;
 y^=x;
 x^=y;
